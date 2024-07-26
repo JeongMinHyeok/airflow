636cb1e713b2d909800cba12a6ac0a94c71c67da
JeongMinHyeok <tlsfk48@gmail.com>
2024-07-26 16:42:49 +0900

    dd

--- plugins/operators/seoul_api_to_csv.py
+++ plugins/operators/seoul_api_to_csv.py
@@ -34,7 +34,7 @@ class SeoulApiToCsvOperator(BaseOperator):
                 end_row += 1000
 
         if not os.path.exists(self.path):
-            os.mkdirs(self.path)
+            os.makedirs(self.path)
             self.log.info('폴더 생성')
         total_row_df.to_csv(self.path + '/' + self.file_name, encoding='utf-8', index=False)
 
